29a5d747c6ed5d519c03086decdf3a95b5534823
Stigi888 <60792494+Stigi888@users.noreply.github.com>
2020-08-03 19:32:21 -0700

    Updates

---
+++ precache-manifest.5cba3725b19d735e2c72e26a73234200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f06ce6396d861669f1d65ff8bd16f2",
+    "url": "/Stigi888/index.html"
+  },
+  {
+    "revision": "c6ba79f8aa6d454c0f18",
+    "url": "/Stigi888/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "8577f9751edd4416fd36",
+    "url": "/Stigi888/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6ba79f8aa6d454c0f18",
+    "url": "/Stigi888/static/js/2.1214f495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stigi888/static/js/2.1214f495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8577f9751edd4416fd36",
+    "url": "/Stigi888/static/js/main.81882af4.chunk.js"
+  },
+  {
+    "revision": "2ae3012ee94ecccaa124",
+    "url": "/Stigi888/static/js/runtime-main.05e11eb1.js"
+  }
+]);
